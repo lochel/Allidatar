8e8aa275639df9efab8f74f6f9eef4a6ee1f1e49
Lennart Ochel <lennart.ochel@outlook.com>
2025-06-13 20:33:32 +0200

    Update upgrade

--- allidatar/client/upgrade.py
+++ allidatar/client/upgrade.py
@@ -2,19 +2,30 @@ import os
 import subprocess
 import sys
 
+from allidatar.version import version_tuple
+
+
+def run_command(command):
+  print(f'[INFO] Running command: {" ".join(command)}')
+  subprocess.run(command, check=True, stdout=subprocess.DEVNULL)
+
 
 def update_and_restart():
   '''
   Update the client by pulling the latest changes from the git repository
   and restart the client if there are new changes.
   '''
-  old_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
-  subprocess.run(['git', 'pull'], check=True)
-  new_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+  current_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
+  print("[INFO] Current version:", version_tuple)
+  print("[INFO] Current hash:", current_hash)
+  run_command(['git', 'pull'])
+  new_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
 
-  if old_hash != new_hash:
+  if current_hash != new_hash:
     print('[INFO] New version detected. Restarting...')
     python = sys.executable
+    run_command([python, '-m', 'build'])
+    run_command([python, '-m', 'pip', 'install', '-e', '.[dev]'])
     os.execv(python, [python] + sys.argv)
   else:
     print('[INFO] Already up to date. No restart needed.')
